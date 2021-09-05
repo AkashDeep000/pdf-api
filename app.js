3545f4a3e898a97510a7384820e71538899cf1ac
AkashDeep000 <akashdeepdas970@gmail.com>
2021-09-05 18:17:22 +0000

    vdiching multer

--- app.js
+++ app.js
@@ -13,37 +13,102 @@ app.use(cors({
 
 import multer from "multer"
 import exec from "await-exec";
-import fs from "fs"
-
+import fs from "fs-extra"
 const host = process.env.HOST
 
+import busboy from "connect-busboy";   // Middleware to handle the file upload https://github.com/mscdex/connect-busboy
+ 
+app.use(busboy({
+    highWaterMark: 0.5 * 1024 * 1024, // Set 0.5MiB buffer
+})); // Insert the busboy middle-ware
+ 
+const uploadPath = '/mnt/file/'; // Register the upload path
+fs.ensureDir(uploadPath); // Make sure that he upload path exits
+ 
+ 
+/**
+ * Create route /upload which handles the post request
+ */
+ /*
+app.route('/upload').post((req, res, next) => {
+ 
+    req.pipe(req.busboy); // Pipe it trough busboy
+ 
+    req.busboy.on('file', (pdfFile, file, filename) => {
+        console.log(`Upload of '${filename}' started`);
+ 
+        // Create a write stream of the new file
+        const fstream = fs.createWriteStream(`${uploadPath}${filename}`);
+        // Pipe it trough
+        file.pipe(fstream);
+ 
+        // On finish of the upload
+        fstream.on('close', () => {
+            console.log(`Upload of '${filename}' finished`);
+            res.redirect('back');
+        });
+    });
+});
 
-const upload = multer({ dest: '/mnt/file/' })
+*/
+
+//const upload = multer({ dest: '/mnt/file/' })
 
 app.use('/file/', express.static('/mnt/file'))
 
-app.post('/upload', upload.single('pdfFile'), async function (req, res, next) {
+app.post('/upload', async (req, res, next) => {
   
   req.socket.setTimeout(10 * 60 * 60 * 1000)
-  req.server.headersTimeout = (10 * 60 * 60 * 1000)
+  
+  req.pipe(req.busboy); 
+  // Pipe it trough busboy
+if (req.busboy) {
+  let formData = new Map();
+  req.busboy.on('field', function(fieldname, val) {
+    formData.set(fieldname, val);
+  });
+  let filename;
+ req.busboy.on('file', (pdfFile, file, filenameClient) => {
+  
+        console.log(`Upload of '${filenameClient}' started`);
+         filename = `${filenameClient.slice(0, -4).replace(/\s+/g, '-').replace(/[{(&$@<>?!%#*^)}]/g, '_')}-${Date.now()}`;
+        // Create a write stream of the new file
+        const fstream = fs.createWriteStream(`${uploadPath}${filename}.pdf`);
+        // Pipe it trough
+        file.pipe(fstream);
+ 
+        // On finish of the upload
+fstream.on('close', () => {
+  console.log(`Upload of '${filename}' finished`);
+});
+return true
+});
+
+req.busboy.on("finish", async () => {
+
+    console.log(formData) // Map { 'name' => 'hi', 'number' => '4' }
+    // here you can do 
+    const pdfLimit = formData.get('pdfLimit') //  '600'
+    const pdfGray = formData.get('pdfGray') //  'true'
+
+    // any other logic with formData here
+
 // console.log(req)
  function getBaseLog(x, y) {
   return Math.log(y) / Math.log(x);
  }
- if (!req.file || !req.body.pdfLimit) {
+ if (!pdfLimit) {
    res.writeHead(404)
-   res.end("No File Given!")
+   res.send("no limit set")
  }
  //has file and limit
- if (req.file && req.body.pdfLimit) {
+ if (pdfLimit) {
    
-   let input = await req.file.path;
-   const pdfGray = req.body.pdfGray;
-   const filename = `${req.file.originalname.slice(0, -4).replace(/\s+/g, '-').replace(/[{(&$@<>?!%#*^)}]/g, '_')}-output-${Date.now()}`;
+   const input = `/mnt/file/${filename}.pdf`;
    
-   const output = `/mnt/file/${filename}.pdf`;
-   const output1 = `/mnt/file/${filename}1.pdf`;
-   const output2 = `/mnt/file/${filename}2.pdf`;
+   const output = `/mnt/file/${filename}-out.pdf`;
+   const output1 = `/mnt/file/${filename}-out1.pdf`;
+   const output2 = `/mnt/file/${filename}-out2.pdf`;
    
    
 //compress into Tow dpi to find perfect dpi
@@ -57,7 +122,7 @@ await Promise.all([fileStats1,fileStats2])
 console.log("fileStats1(25DPI)",(fileStats1.size/1000))
 console.log("fileStats2(50DPI)",(fileStats2.size/1000))
 //Calculating DPI
-const fileLimit = req.body.pdfLimit * 1024;
+const fileLimit = pdfLimit * 1024;
 const a = getBaseLog((25 / 50), (fileStats1.size / fileStats2.size))
 console.log(a)
 const b = (fileStats1.size / Math.pow(25,a))
@@ -67,12 +132,12 @@ console.log(dpi)
 const compressStatus = await compressPdf(input, output, dpi, pdfGray);
 const fileStats = await fs.statSync(output);
 console.log("fileStats(Cal DPI)",fileStats.size/1000)
-if ((req.body.pdfLimit - (fileStats.size/1000)) > 10) {
+if ((pdfLimit - (fileStats.size/1000)) > 10) {
   const newDpi = dpi +1 ;
   const compressStatus = await compressPdf(input, output1, newDpi, pdfGray);
 const fileStats = await fs.statSync(output1);
 console.log("fileStats(Cal DPI Incr 1)",fileStats.size/1000)
-if (req.body.pdfLimit > (fileStats.size/1000)) {
+if (pdfLimit > (fileStats.size/1000)) {
   res.json({
     url: `${host}/file/${filename}1.pdf`,
     size: (fileStats.size/1000).toFixed(2)
@@ -84,19 +149,19 @@ if (req.body.pdfLimit > (fileStats.size/1000)) {
   })
 }
 }
-if ((req.body.pdfLimit - (fileStats.size/1000) < 10) && Math.sign(req.body.pdfLimit - (fileStats.size/1000)) == 1) {
+if ((pdfLimit - (fileStats.size/1000) < 10) && Math.sign(pdfLimit - (fileStats.size/1000)) == 1) {
   res.json({
     url: `${host}/file/${filename}.pdf`,
     size: (fileStats.size/1000).toFixed(2)
   })
   
 }
-if (req.body.pdfLimit < (fileStats.size/1000)) {
+if (pdfLimit < (fileStats.size/1000)) {
   const newDpi = dpi - 1 ;
   const compressStatus = await compressPdf(input, output1, newDpi, pdfGray);
 const fileStats = await fs.statSync(output1);
 console.log("fileStats(Cal DPI Decr 1)",fileStats.size/1000)
-if (req.body.pdfLimit > (fileStats.size/1000)) {
+if (pdfLimit > (fileStats.size/1000)) {
   res.json({
     url: `${host}/file/${filename}1.pdf`,
     size: (fileStats.size/1000).toFixed(2)
@@ -114,7 +179,15 @@ console.log("fileStats(Cal DPI Decr 2)",fileStats.size/1000)
 }
 
  }
- //has file and limit End
+ //has file and limit End            
+            
+            
+  //finished          
+});
+        
+
+  
+}
 })
 
 app.listen(5000,() => {
@@ -140,6 +213,9 @@ async function compressPdf  (input, output, dpiFr, pdfGray) {
   }else{
       try {
         const dpi = Math.ceil(dpiFr)
+    //    console.log("input",input)
+    //    console.log("output",output)
+    //  console.log("dpi",dpi)
     await  exec(
       `gs \ -q -dNOPAUSE -dBATCH -dSAFER \ -sDEVICE=pdfwrite \ -dCompatibilityLevel=1.4 \ -dPDFSETTINGS=/ebook \ -dEmbedAllFonts=true \ -dSubsetFonts=true \ -dAutoRotatePages=/None \ -dNOCIE \ -dNOPSICC \ -dColorAccuracy=0 \ -dDownsampleColorImages=true \ -dColorImageDownsampleType=/Bicubic \ -dColorImageResolution=${dpi} \ -dGrayImageDownsampleType=/Bicubic \ -dGrayImageResolution=${dpi} \ -dMonoImageDownsampleType=/Subsample \ -dMonoImageResolution=${dpi} \ -sOutputFile=${output} \ ${input}`)
         console.log(`Compressed ✓ (${dpi})dpi`)
